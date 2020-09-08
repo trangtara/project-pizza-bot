69f88bd844edf4a57c08c4e0f414c421d98c902a
Trang Tara <trangnusi@gmail.com>
2020-09-08 16:32:34 +0200

    create bot used javascript

--- code/pizzabot.js
+++ code/pizzabot.js
@@ -5,3 +5,55 @@ const pepperoni = "Pepperoni Pizza"
 const pizzaPrice = 80
 
 //Put your Javscript code here:
+
+alert(`Hey! Happy to serve your pizza. On our menu we have ${vegetarian}, ${hawaiian} and ${pepperoni}`);
+
+const validateOrderName = (orderName) => {
+  return orderName === 'vegetarian' || orderName === 'pepperoni' || orderName === 'hawaiian';
+}
+
+const calculateTotalCost = (orderQuantity, pizzaPrice) =>
+   orderQuantity * pizzaPrice
+
+
+const calculateCookingTime = (orderQuantity) => {
+  if (orderQuantity > 0 && orderQuantity <= 2) {
+    return 10
+  } else if (orderQuantity >= 3 && orderQuantity <= 5) {
+    return 15
+  } else {
+    return 20
+  }
+}
+
+const startOrder = () => {
+  let orderName = prompt('Enter the name of the pizza you want to order today.')
+
+  if (orderName && orderName.length > 0) {
+    orderName = orderName.toLowerCase(); 
+  
+      if (validateOrderName(orderName)) {
+        let orderQuantity = prompt(`How many of ${orderName} do you want?`);
+        document.getElementById('complete').innerHTML = `Great, I'll get started on your ${orderName} right away, it will cost ${calculateTotalCost(orderQuantity, pizzaPrice)} kr. The pizza(s) will take ${calculateCookingTime(orderQuantity)} minutes.`
+              
+      } else {
+          alert('Select a pizza from the menu');
+          startOrder();
+      }
+  } 
+      
+}
+
+
+startOrder();
+
+
+
+
+
+
+
+
+
+
+
